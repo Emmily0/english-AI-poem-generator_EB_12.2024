9166770636dd02ff3e311ca98fce2575527ab4e6
Emmily0 <81269634+Emmily0@users.noreply.github.com>
2024-12-14 14:25:45 +0100

    Improved UX of the Project and removed Console Logs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 function displayPoem(response) {
-    console.log("poem generated");
     new Typewriter("#poem", {
         strings: response.data.answer,
         autoStart: true,
-        delay: 50,
+        delay: 40,
         cursor:"",
       });
 }
@@ -17,9 +16,9 @@ let prompt = `User instructions are to generate an English poem about ${instruct
 let context = "You are a romantic Poem expert and love to write short poems inspired by Shakespeare style and wording. Your mission is to generate a 6 lines poem in basic HTML, do not display HTML or /''/ or /``/ at the beginning or ending of the poem and seperate each line with a <br />. Please make sure to follow the User instructions, thank you. Do not include a title in the poem. Sign the end of the poem in with SheCodes AI inside a <strong> element.";
 let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-console.log("Generating poem");
-console.log(`Prompt: ${prompt}`);
-console.log(`Context: ${context}`);
+let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class="generating">⏳Generating a poem about ${instructionsInput.value}...</div>`;
 
 axios.get(apiURL).then(displayPoem);
 }
