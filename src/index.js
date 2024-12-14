639ebe09cdeea8a6f43e6236fbc1ab3f15d8208a
Emmily0 <81269634+Emmily0@users.noreply.github.com>
2024-12-14 13:27:39 +0100

    Added the SheCodes AI API integration and improved the title

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+function displayPoem(response) {
+    console.log("poem generated");
+    new Typewriter("#poem", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 50,
+        cursor:"",
+      });
+}
+
 function generatePoem(event) {
     event.preventDefault();
 
-new Typewriter("#poem", {
-    strings: "Shall I compare thee to a summer's day?",
-    autoStart: true,
-    delay: 50,
-    cursor:"",
-  });
+    let instructionsInput = document.querySelector("#instructions");
+let apiKey = "a4adbd3ete0df14f5fa9050ddd7off63";
+let prompt = `User instructions are to generate an English poem about ${instructionsInput.value}`;
+let context = "You are a romantic Poem expert and love to write short poems inspired by Shakespeare style and wording. Your mission is to generate a 6 lines poem in basic HTML, do not display HTML or /''/ or /``/ at the beginning or ending of the poem and seperate each line with a <br />. Please make sure to follow the User instructions, thank you. Do not include a title in the poem. Sign the end of the poem in with SheCodes AI inside a <strong> element.";
+let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log("Generating poem");
+console.log(`Prompt: ${prompt}`);
+console.log(`Context: ${context}`);
+
+axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
